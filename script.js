468a259f80c7fe7c30f3db475ef6c4fb00b388fa
Vitor <vitormoroli@gmail.com>
2023-01-10 10:07:38 -0300

    tornando responsivo

--- script.js
+++ script.js
@@ -81,36 +81,3 @@ btnResetar.addEventListener('click',resetar)
 
 
 
-// const aumentarSeg = ()=>{
-//     segundos++
-//     seg.innerText = `0${segundos}` 
-//     if(segundos > 9 && segundos < 60){
-//         seg.innerText = `${segundos}` 
-//     }
-//     else if(segundos > 59){
-//         segundos = 0
-//         seg.innerText = `0${segundos}` 
-//     }
-// }
-// const aumentarMin = ()=>{
-//     minutos++
-//     min.innerText = `0${minutos}:`
-//     if(minutos > 9 && minutos < 60){
-//         min.innerText = `${minutos}:`
-//     }
-//     else if(minutos > 59){
-//         minutos = 0
-//         min.innerText = `0${minutos}:`
-//     }
-// }
-// const aumentarHor = ()=>{
-//     horas++
-//     hor.innerText = `0${horas}:`
-//     if(horas > 9 && horas < 60){
-//         hor.innerText = `${horas}:`
-//     }
-//     else if(horas > 59){
-//         horas = 0
-//         hor.innerText = `0${horas}:`
-//     }
-// }
